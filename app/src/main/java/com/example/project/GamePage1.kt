db80ee9ba3053856446c53adfd56544062a25ba7
jhst5 <123>
2021-05-20 16:18:54 +0800

    0520專題雛形+音檔+AI辨識

--- app/src/main/java/com/example/project/GamePage1.kt
+++ app/src/main/java/com/example/project/GamePage1.kt
@@ -1,6 +1,7 @@
 package com.example.project
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,11 +9,13 @@ import kotlinx.android.synthetic.main.activity_game_page1.*
 
 
 class GamePage1 : AppCompatActivity(),View.OnClickListener {
+    lateinit var mper: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_page1)
         btnwake.setOnClickListener(this)
         btnsleep.setOnClickListener(this)
+        mper = MediaPlayer()
     }
 
     override fun onClick(v: View) {
@@ -22,6 +25,38 @@ class GamePage1 : AppCompatActivity(),View.OnClickListener {
         }else if(v.id.equals(R.id.btnsleep)){
             intent = Intent(this@GamePage1, GamePage2_1::class.java)
             startActivity(intent)
+        }
+    }
+    fun StartPlay(v: View){
+        mper.reset()
+
+        if(v.id.equals(R.id.btnPlayer)){
+            mper = MediaPlayer.create(this, R.raw.tcyang)
+            mper.start()
+        }
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        if(mper != null) {
+            mper.release()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if(mper != null && mper.isPlaying()){
+            mper.pause()
+        }
+        else{
+            mper.reset()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(mper != null){
+            mper.start()
         }
     }
 }
