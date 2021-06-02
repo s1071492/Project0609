c018263a031b9e1cda9f3dcc7a7de08382ebc6d7
jhst5 <123>
2021-06-02 16:55:20 +0800

    0602

--- app/src/main/java/com/example/project/GamePage4.kt
+++ app/src/main/java/com/example/project/GamePage4.kt
@@ -5,8 +5,15 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_game_page2.*
 import kotlinx.android.synthetic.main.activity_game_page4.*
-
+import kotlinx.android.synthetic.main.activity_game_page4.btnAI
+import kotlinx.android.synthetic.main.activity_game_page4.btnAngry
+import kotlinx.android.synthetic.main.activity_game_page4.btnHappy
+import kotlinx.android.synthetic.main.activity_game_page4.btnSad
+import kotlinx.android.synthetic.main.activity_game_page4.txvAnswer
+import kotlinx.android.synthetic.main.activity_game_page4.txvIsRight
 
 
 class GamePage4 : AppCompatActivity(),View.OnClickListener {
@@ -25,21 +32,24 @@ class GamePage4 : AppCompatActivity(),View.OnClickListener {
     override fun onClick(v: View) {
         if(v.id.equals(R.id.btnHappy)){
             txvAnswer.text=btnHappy.text
+            Toast.makeText(this, "您選擇的是開心,回答正確!", Toast.LENGTH_SHORT).show();
             txvIsRight.text="正確"
         }else if(v.id.equals(R.id.btnAI)){
             if(txvIsRight.text.equals("正確"))
             {
                 intent = Intent(this@GamePage4, DrawCheck::class.java)
                 intent.putExtra("表情",txvAnswer.text)
-                intent.putExtra("頁數",4)
+                intent.putExtra("頁數",2)
                 startActivity(intent)
             }else{
-                txvIsRight.text="請先回答正確再進行試試看!"
+                Toast.makeText(this, "請先回答正確再進行試試看!", Toast.LENGTH_SHORT).show();
             }
         }else if(v.id.equals(R.id.btnSad)){
+            Toast.makeText(this, "您選擇的是難過,回答錯誤!", Toast.LENGTH_SHORT).show();
             txvAnswer.text=btnSad.text
             txvIsRight.text="錯誤!請再看一次故事"
         }else if(v.id.equals(R.id.btnAngry)){
+            Toast.makeText(this, "您選擇的是生氣,回答錯誤!", Toast.LENGTH_SHORT).show();
             txvAnswer.text=btnAngry.text
             txvIsRight.text="錯誤!請再看一次故事"
         }
--- app/src/main/java/com/example/project/GamePage5.kt
+++ app/src/main/java/com/example/project/GamePage5.kt
@@ -5,7 +5,9 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_game_page2.*
+import kotlinx.android.synthetic.main.activity_game_page4.*
 import kotlinx.android.synthetic.main.activity_game_page5.*
 import kotlinx.android.synthetic.main.activity_game_page5.btnAI
 import kotlinx.android.synthetic.main.activity_game_page5.btnAngry
@@ -30,21 +32,24 @@ class GamePage5 : AppCompatActivity(),View.OnClickListener {
     override fun onClick(v: View) {
         if(v.id.equals(R.id.btnSad)){
             txvAnswer.text=btnSad.text
+            Toast.makeText(this, "您選擇的是難過,回答正確!", Toast.LENGTH_SHORT).show();
             txvIsRight.text="正確"
         }else if(v.id.equals(R.id.btnAI)){
             if(txvIsRight.text.equals("正確"))
             {
                 intent = Intent(this@GamePage5, DrawCheck::class.java)
                 intent.putExtra("表情",txvAnswer.text)
-                intent.putExtra("頁數",5)
+                intent.putExtra("頁數",2)
                 startActivity(intent)
             }else{
-                txvIsRight.text="請先回答正確再進行試試看!"
+                Toast.makeText(this, "請先回答正確再進行試試看!", Toast.LENGTH_SHORT).show();
             }
         }else if(v.id.equals(R.id.btnHappy)){
+            Toast.makeText(this, "您選擇的是開心,回答錯誤!", Toast.LENGTH_SHORT).show();
             txvAnswer.text=btnHappy.text
             txvIsRight.text="錯誤!請再看一次故事"
         }else if(v.id.equals(R.id.btnAngry)){
+            Toast.makeText(this, "您選擇的是生氣,回答錯誤!", Toast.LENGTH_SHORT).show();
             txvAnswer.text=btnAngry.text
             txvIsRight.text="錯誤!請再看一次故事"
         }
